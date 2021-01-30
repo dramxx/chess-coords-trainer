ba6055cc4b6a77f4f1f571e30416410d09834ecc
dramxx <drahoslav.madar@gmail.com>
2021-01-30 01:00:31 +0100

    adding todos

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import React from "react";
 import GameManager from "./components/GameManager";
 import "./App.css";
 
+// TODO:
+// => add game start preloader
+// => store high score in localStorage
+// => return feedback on current score based on high score
+
 const App = () => {
   return (
     <div className="App flex">
